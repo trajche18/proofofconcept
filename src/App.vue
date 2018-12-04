10cb64bfc26d6cfcb66c9cbc2bf61d4e6d27d898
trajche18 <fateful.nl@gmail.com>
2018-12-05 00:05:04 +0100

    message

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="sideNav" fixed>
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>insert_comment</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>Blogs</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar class="primary">
+      <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav"
+      class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>Welcome To My Blogs</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon left>insert_comment</v-icon>
+          Blogs
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sideNav: false
+
+    }
+  },
+  name: 'App'
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/es5/util/colors'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.purple,
+    secondary: colors.green.lighten1,
+    accent: colors.shades.black,
+    error: colors.red.accent3
+  }
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
